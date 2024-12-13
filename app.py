99dd25cedf6a5fa492da510b055450bec5c6af5e
SamueleCreti <samu.2006.br@gmail.com>
2024-12-13 12:36:48 +0000

    COMMIT 1

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+lista=(("PatatineS.C", "SC03", 2), ("Philadelphia", "SC04", 3), ("Prosciutto Crudo", "SC05", 2.20))
 
-app.run()
+@app.route("/")
+def homepage():
+    return render_template("home.html", titolo="Home")
+
+@app.route("/detail")
+def detail():
+    return render_template("detail.html", titolo="Detail", lista=lista)
+
+@app.route("/scaffale")
+def scaffale():
+    return render_template("dettagliScaffale.html", titolo="Dettagli Scaffale")
+
+if __name__ == '__main__': 
+    app.run(debug=True)
